4f08cdafa62f058536ace98eb1eccbf7013ac877
Sherlin Toriz Pérez <sherlintp09@gmail.com>
2022-02-04 09:26:12 -0600

    Imagenes en JS

--- src/index.js
+++ src/index.js
@@ -3,4 +3,25 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
-console.log('Happy hacking :)')
+
+//crear una img
+const createImageNode = () => {
+
+    const container = document.createElement('div');
+    container.className = "p-4";
+
+    const imagen = document.createElement('img')
+    imagen.className = "mx-auto";
+    imagen.width = '320'
+    imagen.src = "https://randomfox.ca/images/2.jpg"; //TODO
+
+    container.appendChild(imagen);
+
+    return imagen;
+};
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images")
+
+
+mountNode.appendChild(nuevaImagen);
